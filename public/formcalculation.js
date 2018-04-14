ae0635090a81871028ecee657e2788dc2e286a6c
ameba23 <ameba23@systemli.org>
2018-04-14 20:27:35 +0200

    bootstrap, copy to clipboard, other bits

--- public/formcalculation.js
+++ public/formcalculation.js
@@ -1,4 +1,32 @@
-        
+ function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+}
+       
 function calculateAmount(theRate,weekAv)
 {
     
@@ -11,9 +39,15 @@ function calculateAmount(theRate,weekAv)
         days = parseFloat(quantity.value);
     }
     var pay = (days * theRate) / weekAv;
-    //display the result
-    var divobj = document.getElementById('totalPrice');
-    //divobj.style.display='block';
-    divobj.innerHTML = "days per week @ €" + theRate + " per day. This weeks pay is BTC "+pay.toFixed(9);
+    return pay.toFixed(9)
 }
 
+function outputAmount(theRate,weekAv)
+{
+  
+  //display the result
+  var divobj = document.getElementById('totalPrice');
+  //divobj.style.display='block';
+  divobj.innerHTML = calculateAmount(theRate,weekAv) 
+  //+ " <button onclick="copyTextToClipboard(pay.toFixed(9))"> copy to clipboard </button>";
+}
